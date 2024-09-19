6fecdadc6d4a917d5638f15fb1baf5d6fa7a9dc3
Ana Cecilia Porcidonio <cecilia.pordidonio@gmail.com>
2024-09-19 15:24:09 -0300

    first commit

---
+++ Hora de acordar.java
@@ -0,0 +1,27 @@
+Hora de acordar
+
+Início
+
+1. Acordo;
+2. Abro os olhos;
+3. Levanto da cama;
+
+4. É dia de Home office? Sim
+    1. Vou ao banheiro;
+    2. Escovo os dentes;
+    3. Lavo o rosto;
+    4. Troco de roupa;
+    5. Tomo café da manhã
+    6. Ligo o computador para trabalhar;
+
+5. É dia de Home office? Não
+    1. Vou ao banheiro;
+    2. Tomo banho;
+    4. Me arrumo para o trabalho;
+    5. Tomo café da manhã;
+    6. Escovo os dentes;
+    7. Pego o ônibus;
+    8. Chego ao trabalho;
+    9. Ligo o computador para trabalhar;
+
+Fim
